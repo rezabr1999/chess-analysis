bcef1d4443b2af90fbba5f97b4527ff8267d574c
Reza Br <reza.linusx@gmail.com>
2024-09-17 14:20:18 +0330

    functionality to go to next and previous moves

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,41 @@ import { parse } from "@mliebelt/pgn-parser";
 function App() {
   const [pgn, setPgn] = useState<string>("");
   const [game, setGame] = useState<Chess>(new Chess());
+  const [moves, setMoves] = useState<string[]>([]);
+  const [currentMove, setCurrentMove] = useState<number>(0);
 
-  const playGame = () => {
+  const addGame = () => {
     let gameNotations = parse(pgn, { startRule: "game" });
-    gameNotations.moves.forEach((move: any, index: number) => {
-      setTimeout(() => {
-        setGame((prevGame) => {
-          const newGame = new Chess(prevGame.fen());
-          newGame.move(move.notation.notation);
-          return newGame;
-        });
-      }, 1000 * index);
-    });
+    const parsedMoves = gameNotations.moves.map(
+      (move: any) => move.notation.notation
+    );
+    setMoves(parsedMoves);
+    setGame(new Chess());
+    setCurrentMove(0);
+    setPgn("");
   };
+
+  function nextMove() {
+    if (currentMove < moves.length) {
+      const newGame = new Chess();
+      for (let i = 0; i <= currentMove; i++) {
+        newGame.move(moves[i]);
+      }
+      setGame(newGame);
+      setCurrentMove(currentMove + 1);
+    }
+  }
+
+  function prevMove() {
+    if (currentMove > 0) {
+      const newGame = new Chess();
+      for (let i = 0; i < currentMove - 1; i++) {
+        newGame.move(moves[i]);
+      }
+      setGame(newGame);
+      setCurrentMove(currentMove - 1);
+    }
+  }
 
   return (
     <div className="p-5 bg-gradient-to-b from-gray-800 to-gray-900 min-h-screen flex flex-col items-center justify-center">
@@ -35,7 +57,7 @@ function App() {
           onChange={(e) => setPgn(e.target.value)}
         />
         <button
-          onClick={playGame}
+          onClick={addGame}
           title="add game"
           className="bg-purple-600 p-3 w-full rounded-lg text-white font-medium transition-all duration-300 hover:bg-purple-700"
         >
@@ -44,8 +66,27 @@ function App() {
       </div>
 
       <div className="mt-10 w-full max-w-md">
-        <div className="bg-gray-800 shadow-xl rounded-xl overflow-hidden">
+        <div className="bg-gray-800 shadow-xl rounded-xl overflow-hidden flex flex-col gap-3">
           <Chessboard position={game.fen()} />
+          <div className="flex gap-4 p-3">
+            <button
+              onClick={prevMove}
+              title="prev"
+              type="button"
+              className="p-4 bg-emerald-600 rounded-lg flex-1"
+            >
+              Prev Move
+            </button>
+
+            <button
+              onClick={nextMove}
+              title="next"
+              type="button"
+              className="p-4 bg-sky-700 rounded-lg flex-1"
+            >
+              Next Move
+            </button>
+          </div>
         </div>
       </div>
     </div>
